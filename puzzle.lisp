912f1bcb20d78184bd20a75b9f7a267725451402
Miguel Rodrigues <33757448+nhenheca@users.noreply.github.com>
2022-12-06 15:22:41 +0000

    3

--- puzzle.lisp
+++ puzzle.lisp
@@ -1,140 +1,86 @@
 
-CL-USER 1 > (get-arco-na-posicao 2 3 (get-arcos-horizontais (no-teste)))
+;;;MIGUEL ########################################################
 
-Error: Undefined operator GET-ARCO-NA-POSICAO in form (GET-ARCO-NA-POSICAO 2 3 (GET-ARCOS-HORIZONTAIS (NO-TESTE))).
-  1 (continue) Try invoking GET-ARCO-NA-POSICAO again.
-  2 Return some values from the form (GET-ARCO-NA-POSICAO 2 3 (GET-ARCOS-HORIZONTAIS (NO-TESTE))).
-  3 Try invoking something other than GET-ARCO-NA-POSICAO with the same arguments.
-  4 Set the symbol-function of GET-ARCO-NA-POSICAO to another function.
-  5 Set the macro-function of GET-ARCO-NA-POSICAO to another function.
-  6 (abort) Return to top loop level 0.
+(defun heuristica (no)
+  0
+)
 
-Type :b for backtrace or :c <option number> to proceed.
-Type :bug-form "<subject>" for a bug report template or :? for other options.
+ "Retorna um tabuleiro 3x3 (3 arcos na vertical por 3 arcos na horizontal) Profundidade Heuristica Pai"
+(defun no-teste ()
+ (list (tabuleiro-teste) 0 (heuristica (tabuleiro-teste)) nil)
+)
 
-CL-USER 2 : 1 > (get-arco-na-posicao 2 3 (get-arcos-horizontais (no-teste)))
+;;; SELETORES ;;;
+(defun tabuleiro (no)
+ (car no)
+)
 
-Error: NTHCDR reached dotted end of list 0 before 2 elements.
-  1 (abort) Return to debug level 1.
-  2 Return to top loop level 0.
+;;;MIGUEL #########################################################
 
-Type :b for backtrace or :c <option number> to proceed.
-Type :bug-form "<subject>" for a bug report template or :? for other options.
+;;; Tabuleiro
+ "Retorna um tabuleiro 3x3 (3 arcos na vertical por 3 arcos na horizontal)"
+(defun tabuleiro-teste ()
+    '(
+        ((0 0 0) (0 0 1) (0 1 1) (0 0 1))
+        ((0 0 0) (0 1 1) (1 0 1) (0 1 1))
+    )
+)
 
-CL-USER 3 : 2 > (get-arcos-verticais (no-teste))
-((0 0 0) (0 1 1) (1 0 1) (0 1 1))
+;;; SELETORES ;;;
+;;;(get-arcos-horizontais (no-teste))
+;;;((0 0 0) (0 0 1) (0 1 1) (0 0 1))
+(defun get-arcos-horizontais(no)
+ (car (tabuleiro no))
+)
 
-CL-USER 4 : 2 > (get-arcos-horizontais (no-teste))
-((0 0 0) (0 0 1) (0 1 1) (0 0 1))
+;;;(get-arcos-verticais (no-teste))
+;;;((0 0 0) (0 1 1) (1 0 1) (0 1 1))
+(defun get-arcos-verticais(no)
+ (car (cdr (tabuleiro no)))
+)
 
-CL-USER 5 : 2 > (get-arco-na-posicao 2 3 (get-arcos-horizontais (no-teste)))
+;;(get-arco-na-posicao 2 3 (get-arcos-horizontais (no-teste)))
+;;1
+(defun get-arco-na-posicao (x y l)
+ (nth (- y 1)(nth (- x 1) l))
+)
 
-Error: NTHCDR reached dotted end of list 0 before 2 elements.
-  1 (abort) Return to debug level 2.
-  2 Return to debug level 1.
-  3 Return to top loop level 0.
+;;; AUXILIARES ;;;
+;;;(substituir 1 (car (get-arcos-horizontais (no-teste))))
+;;;(1 0 0)
+;;;(substituir 2 (car (get-arcos-verticais (no-teste))) 2)
+;;;(0 2 0)
+(defun substituir(x l &optional (y 1))
+ (cond
+  ((= x 1) (cons y (cdr l)))
+  (t (cons (car l) (substituir (1- x) (cdr l) y)))
+ )
+)
 
-Type :b for backtrace or :c <option number> to proceed.
-Type :bug-form "<subject>" for a bug report template or :? for other options.
+;;;(arco-na-posicao 2 2 (get-arcos-horizontais (no-teste)))
+;;;((0 0 0) (0 1 1) (0 1 1) (0 0 1))
+;;;(arco-na-posicao 4 1 (get-arcos-verticais (tabuleiro-teste)))
+;;;((0 0 0) (0 1 1) (1 0 1) (1 1 1))
+(defun arco-na-posicao (x y l &optional (z 1))
+ (cond
+  ((= x 1) (cons (substituir y (car l) z) (cdr l)))
+  (t (cons (car l) (arco-na-posicao (1- x) y (cdr l) z)))
+ )
+)
 
-CL-USER 6 : 3 > (nth (- x 1)(tabuleiro no))
+;;; OPERADORES ;;;
+(defun arco-horizontal (pos i l &optional (z 1))
+ (cond
+  ((equal nil (get-arco-na-posicao pos i (get-arcos-horizontais (tabuleiro-teste)))) nil)
+  ((= 1 (get-arco-na-posicao pos i (get-arcos-horizontais (tabuleiro-teste)))) nil)
+  (t (append (arco-na-posicao pos i (get-arcos-horizontais (tabuleiro-teste)) z)  l))
+ )
+)
 
-Error: The variable X is unbound.
-  1 (continue) Try evaluating X again.
-  2 Return the value of :X instead.
-  3 Specify a value to use this time instead of evaluating X.
-  4 Specify a value to set X to.
-  5 (abort) Return to debug level 3.
-  6 Return to debug level 2.
-  7 Return to debug level 1.
-  8 Return to top loop level 0.
-
-Type :b for backtrace or :c <option number> to proceed.
-Type :bug-form "<subject>" for a bug report template or :? for other options.
-
-CL-USER 7 : 4 > (nth (- 2 1)(tabuleiro no))
-
-Error: The variable NO is unbound.
-  1 (continue) Try evaluating NO again.
-  2 Return the value of :NO instead.
-  3 Specify a value to use this time instead of evaluating NO.
-  4 Specify a value to set NO to.
-  5 (abort) Return to debug level 4.
-  6 Return to debug level 3.
-  7 Return to debug level 2.
-  8 Return to debug level 1.
-  9 Return to top loop level 0.
-
-Type :b for backtrace or :c <option number> to proceed.
-Type :bug-form "<subject>" for a bug report template or :? for other options.
-
-CL-USER 8 : 5 > (nth (- x 1)(tabuleiro (no-teste)))
-
-Error: The variable X is unbound.
-   1 (continue) Try evaluating X again.
-   2 Return the value of :X instead.
-   3 Specify a value to use this time instead of evaluating X.
-   4 Specify a value to set X to.
-   5 (abort) Return to debug level 5.
-   6 Return to debug level 4.
-   7 Return to debug level 3.
-   8 Return to debug level 2.
-   9 Return to debug level 1.
-  10 Return to top loop level 0.
-
-Type :b for backtrace or :c <option number> to proceed.
-Type :bug-form "<subject>" for a bug report template or :? for other options.
-
-CL-USER 9 : 6 > (nth (- 2 1)(tabuleiro (no-teste)))
-((0 0 0) (0 1 1) (1 0 1) (0 1 1))
-
-CL-USER 10 : 6 > (get-arco-na-posicao 2 3 (get-arcos-horizontais (no-teste)))
-
-Error: NTHCDR reached dotted end of list 0 before 2 elements.
-  1 (abort) Return to debug level 6.
-  2 Return to debug level 5.
-  3 Return to debug level 4.
-  4 Return to debug level 3.
-  5 Return to debug level 2.
-  6 Return to debug level 1.
-  7 Return to top loop level 0.
-
-Type :b for backtrace or :c <option number> to proceed.
-Type :bug-form "<subject>" for a bug report template or :? for other options.
-
-CL-USER 11 : 7 > (nth (- 2 1)(get-arcos-horizontais(tabuleiro (no-teste))))
-0
-
-CL-USER 12 : 7 > (get-arcos-horizontais (no-teste))
-((0 0 0) (0 0 1) (0 1 1) (0 0 1))
-Error while reading: Unmatched right parenthesis.
-
-CL-USER 13 : 7 > )
-
-Error: Unmatched right parenthesis.
-  1 (abort) Return to debug level 7.
-  2 Return to debug level 6.
-  3 Return to debug level 5.
-  4 Return to debug level 4.
-  5 Return to debug level 3.
-  6 Return to debug level 2.
-  7 Return to debug level 1.
-  8 Return to top loop level 0.
-
-Type :b for backtrace or :c <option number> to proceed.
-Type :bug-form "<subject>" for a bug report template or :? for other options.
-Error while reading: Unmatched right parenthesis.
-
-CL-USER 13 : 8 > (get-arcos-horizontais (no-teste))
-((0 0 0) (0 0 1) (0 1 1) (0 0 1))
-
-CL-USER 14 : 8 > (nth (- 2 1)(get-arcos-horizontais (no-teste)))
-(0 0 1)
-
-CL-USER 15 : 8 > (nth (- 3 1)(nth (- 2 1)(get-arcos-horizontais (no-teste))))
-1
-
-CL-USER 16 : 8 > (get-arco-na-posicao 2 3 (get-arcos-horizontais (no-teste)))
-1
-
-CL-USER 17 : 8 > 
+(defun arco-vertical (pos i l &optional (z 1))
+ (cond
+  ((equal nil (get-arco-na-posicao pos i (get-arcos-verticais (tabuleiro-teste)))) nil)
+  ((= 1 (get-arco-na-posicao pos i (get-arcos-verticais (tabuleiro-teste)))) nil)
+  (t (append (arco-na-posicao pos i (get-arcos-verticais (tabuleiro-teste)) z)  l))
+ )
+)
